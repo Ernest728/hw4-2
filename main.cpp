a5b7b6d52f87dca4b01688b5d0b5fb36c41b63ec
Ernest728 <41069663+Ernest728@users.noreply.github.com>
2023-05-03 00:47:34 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,148 @@
+#include "mbed.h"
+#include "MQTTNetwork.h"
+#include "MQTTmbed.h"
+#include "MQTTClient.h"
+// Sensors drivers present in the BSP library
+#include "stm32l475e_iot01_tsensor.h"
+#include "stm32l475e_iot01_hsensor.h"
+#include "stm32l475e_iot01_psensor.h"
+#include "stm32l475e_iot01_magneto.h"
+#include "stm32l475e_iot01_gyro.h"
+#include "stm32l475e_iot01_accelero.h"
+
+
+
+// GLOBAL VARIABLES
+WiFiInterface *wifi;
+InterruptIn btn2(BUTTON1);
+//InterruptIn btn3(SW3);
+volatile int message_num = 0;
+volatile int arrivedcount = 0;
+volatile bool closed = false;
+float sensor_value[3] = {0};
+int16_t MegDataXYZ[3] = {0};
+int16_t pDataXYZ[3] = {0};
+float pGyroDataXYZ[3] = {0};
+
+const char* topic = "Mbed";
+
+Thread mqtt_thread(osPriorityHigh);
+EventQueue mqtt_queue(64 * EVENTS_EVENT_SIZE);
+
+void messageArrived(MQTT::MessageData& md) {
+    MQTT::Message &message = md.message;
+    char msg[300];
+    sprintf(msg, "Message arrived: QoS%d, retained %d, dup %d, packetID %d\r\n", message.qos, message.retained, message.dup, message.id);
+    printf(msg);
+    ThisThread::sleep_for(100ms);
+    char payload[300];
+    sprintf(payload, "Payload %.*s\r\n", message.payloadlen, (char*)message.payload);
+    printf(payload);
+    ++arrivedcount;
+}
+
+void publish_message(MQTT::Client<MQTTNetwork, Countdown>* client) {
+    message_num++;
+    MQTT::Message message;
+    char buff[100];
+    sensor_value[0] = BSP_TSENSOR_ReadTemp();
+    sensor_value[1] = BSP_HSENSOR_ReadHumidity();
+    sensor_value[2] = BSP_PSENSOR_ReadPressure();
+    sprintf(buff, "#%d Machine Condition Temp: %.2f DegC Humid: %.2f %% Pressure: %.2f mBar", \
+     message_num, sensor_value[0], sensor_value[1], sensor_value[2]);
+    message.qos = MQTT::QOS0;
+    message.retained = false;
+    message.dup = false;
+    message.payload = (void*) buff;
+    message.payloadlen = strlen(buff) + 1;
+    int rc = client->publish(topic, message);
+
+    printf("rc:  %d\r\n", rc);
+}
+
+void close_mqtt() {
+    closed = true;
+}
+
+int main() {
+    
+    printf("Start sensor init\n");
+    BSP_TSENSOR_Init();
+    BSP_HSENSOR_Init();
+    BSP_PSENSOR_Init();
+
+    BSP_MAGNETO_Init();
+    BSP_GYRO_Init();
+    BSP_ACCELERO_Init();
+    wifi = WiFiInterface::get_default_instance();
+    if (!wifi) {
+            printf("ERROR: No WiFiInterface found.\r\n");
+            return -1;
+    }
+
+
+    printf("\nConnecting to %s...\r\n", MBED_CONF_APP_WIFI_SSID);
+    int ret = wifi->connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
+    if (ret != 0) {
+            printf("\nConnection error: %d\r\n", ret);
+            return -1;
+    }
+
+
+    NetworkInterface* net = wifi;
+    MQTTNetwork mqttNetwork(net);
+    MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
+
+    //TODO: revise host to your IP
+    const char* host = "192.168.134.169";
+    const int port=1883;
+    printf("Connecting to TCP network...\r\n");
+    printf("address is %s/%d\r\n", host, port);
+
+    int rc = mqttNetwork.connect(host, port);//(host, 1883);
+    if (rc != 0) {
+            printf("Connection error.");
+            return -1;
+    }
+    printf("Successfully connected!\r\n");
+
+    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+    data.MQTTVersion = 3;
+    data.clientID.cstring = "Mbed";
+
+    if ((rc = client.connect(data)) != 0){
+            printf("Fail to connect MQTT\r\n");
+    }
+    if (client.subscribe(topic, MQTT::QOS0, messageArrived) != 0){
+            printf("Fail to subscribe\r\n");
+    }
+    
+    int num = 0;
+    while (num != 5) {
+            client.yield(100);
+            ++num;
+    }
+    ThisThread::sleep_for(1s);
+    mqtt_thread.start(callback(&mqtt_queue, &EventQueue::dispatch_forever));
+    
+    while (1) {
+            if (closed) break;
+            mqtt_queue.call(publish_message, &client);
+            client.yield(500);
+            ThisThread::sleep_for(1000ms);
+    }
+
+    printf("Ready to close MQTT Network......\n");
+
+    if ((rc = client.unsubscribe(topic)) != 0) {
+            printf("Failed: rc from unsubscribe was %d\n", rc);
+    }
+    if ((rc = client.disconnect()) != 0) {
+    printf("Failed: rc from disconnect was %d\n", rc);
+    }
+
+    mqttNetwork.disconnect();
+    printf("Successfully closed!\n");
+
+    return 0;
+}
